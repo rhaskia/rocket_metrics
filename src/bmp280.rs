e76af616f39ad5eb7db161c0e293bbaf6be88351
rhaskia <rhaskiagamedev@gmail.com>
2024-05-05 15:51:19 +1200

    small changes

--- src/bmp280.rs
+++ src/bmp280.rs
@@ -19,18 +19,18 @@ pub struct Bmp280Calibration {
 impl Bmp280Calibration {
     pub fn new(i2c: &mut I2c) -> Result<Self, arduino_hal::i2c::Error> {
         Ok(Bmp280Calibration {
-            dig_t1: read16(i2c, 0x88)?,
-            dig_t2: read_s16(i2c, 0x8A)?,
-            dig_t3: read_s16(i2c, 0x8C)?,
-            dig_p1: read16(i2c, 0x8E)?,
-            dig_p2: read_s16(i2c, 0x90)?,
-            dig_p3: read_s16(i2c, 0x92)?,
-            dig_p4: read_s16(i2c, 0x94)?,
-            dig_p5: read_s16(i2c, 0x96)?,
-            dig_p6: read_s16(i2c, 0x98)?,
-            dig_p7: read_s16(i2c, 0x9A)?,
-            dig_p8: read_s16(i2c, 0x9C)?,
-            dig_p9: read_s16(i2c, 0x9E)?,
+            dig_t1: read16_le(i2c, 0x88)?,
+            dig_t2: read_s16_le(i2c, 0x8A)?,
+            dig_t3: read_s16_le(i2c, 0x8C)?,
+            dig_p1: read16_le(i2c, 0x8E)?,
+            dig_p2: read_s16_le(i2c, 0x90)?,
+            dig_p3: read_s16_le(i2c, 0x92)?,
+            dig_p4: read_s16_le(i2c, 0x94)?,
+            dig_p5: read_s16_le(i2c, 0x96)?,
+            dig_p6: read_s16_le(i2c, 0x98)?,
+            dig_p7: read_s16_le(i2c, 0x9A)?,
+            dig_p8: read_s16_le(i2c, 0x9C)?,
+            dig_p9: read_s16_le(i2c, 0x9E)?,
         })
     }
 }
@@ -54,19 +54,41 @@ fn read_s16(i2c: &mut I2c, reg: u8) -> Result<i16, arduino_hal::i2c::Error> {
     Ok(val)
 }
 
+fn read16_le(i2c: &mut I2c, reg: u8) -> Result<u16, arduino_hal::i2c::Error> {
+    let mut buf = [0u8; 2];
+    i2c.write_read(0x76, &[reg], &mut buf);
+    // Combine the two bytes into a 16-bit unsigned integer
+    let val = ((buf[1] as u16) << 8) | (buf[0] as u16);
+    
+    Ok(val)
+}
+
+fn read_s16_le(i2c: &mut I2c, reg: u8) -> Result<i16, arduino_hal::i2c::Error> {
+    let mut buf = [0u8; 2];
+    i2c.write_read(0x76, &[reg], &mut buf);
+
+    // Combine the two bytes into a 16-bit signed integer
+    let val = ((buf[1] as i16) << 8) | (buf[0] as i16);
+    
+    Ok(val)
+}
+
 pub struct Bmp280 {
     t_fine: i32,
     calib: Bmp280Calibration,
+    ground_pressure: f32,
 }
 
 impl Bmp280 {
     pub fn new(calib: Bmp280Calibration) -> Self {
          Self {
+             ground_pressure:0.0,
              calib, t_fine: 0,
          }
     }
 
     pub fn compensate_temperature(&mut self, adc_t: i32) -> f32 {
+        let adc_t = adc_t >> 4;
         let t1 = self.calib.dig_t1 as i32;
         let t2 = self.calib.dig_t2 as i32;
         let t3 = self.calib.dig_t3 as i32;
@@ -83,12 +105,13 @@ impl Bmp280 {
     pub fn compensate_pressure(&mut self, adc_p: i32) -> f32 {
         let var1 = (self.t_fine as i64) - 128000;
 
-        let var2 = var1 * var1 * self.calib.dig_p6 as i64;
-        let var2 = var2 + ((var1 * self.calib.dig_p5 as i64) << 17);
-        let var2 = var2 + ((self.calib.dig_p4 as i64) << 35);
+        let var2 = var1 * var1 * self.calib.dig_p6 as i64
+            + ((var1 * self.calib.dig_p5 as i64) << 17)
+            + ((self.calib.dig_p4 as i64) << 35);
 
-        let var1 = ((var1 * var1 * (self.calib.dig_p3 as i64) >> 8) + ((var1 * (self.calib.dig_p2 as i64)) << 12));
-        let var1 = ((((1i64) << 47) + var1) * (self.calib.dig_p1 as i64)) >> 33;
+        let var1 = (((var1 * var1 * (self.calib.dig_p3 as i64) >> 8) + ((var1 * (self.calib.dig_p2 as i64)) << 12))
+            * ((1i64 << 47) + var1)
+            * (self.calib.dig_p1 as i64)) >> 33;
 
         let p: i64 = 1048576 - adc_p as i64;
         let p = (((p << 31) - var2) * 3125) / var1;
@@ -101,4 +124,31 @@ impl Bmp280 {
         p as f32 / 256000.
     }
 
+    // pub fn zero(&mut self, adc_p: i32) -> f32 {
+    //     self.ground_pressure = self.compensate_pressure(adc_p) * 1000.;
+    //
+    //     self.ground_pressure
+    // }
+
+    pub fn altitude_m_relative(&mut self, adc_p: i32, sea_level_pa: f32) -> f32 {
+        let pressure = self.compensate_pressure(adc_p) * 1000.;
+
+        let altitude = 44330. * (1. - pow_float(pressure / sea_level_pa, 0.1903));
+        altitude
+    }
+
+    pub fn altitude_m(&mut self, adc_p: i32) -> f32 {
+        let pressure = self.ground_pressure;
+
+        self.altitude_m_relative(adc_p, pressure)
+    }
+
+}
+
+fn pow_float(base: f32, exponent: f32) -> f32 {
+    let base_as_u32 = base.to_bits();
+    let exponent_as_u32 = exponent.to_bits();
+
+    let product_as_u32 = base_as_u32.wrapping_mul(exponent_as_u32);
+    f32::from_bits(product_as_u32)
 }
--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,6 @@ unsafe fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
 
-    let mut led = pins.d13.into_output();
     let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
     let mut i2c = arduino_hal::I2c::new(
         dp.TWI,
@@ -22,15 +21,15 @@ unsafe fn main() -> ! {
 
     let calib = Bmp280Calibration::new(&mut i2c).unwrap();
     let mut bmp = bmp280::Bmp280::new(calib);
+    
     i2c.write(0x76, &[0xF4, 0x3F]).unwrap();
-    let mut accel = [0; 3];
     
     loop {
         let adc_t = read24(&mut i2c, 0xFA);
         let adc_p = read24(&mut i2c, 0xF7);
         let t = bmp.compensate_temperature(adc_t as i32);
-        let p = bmp.compensate_pressure(adc_p as i32);
-        ufmt::uwriteln!(&mut serial, "{:?}", p as i32);
+        let p = bmp.altitude_m(adc_p as i32);
+        ufmt::uwriteln!(&mut serial, "{:?}", (p * 100.) as i32);
 
         arduino_hal::delay_ms(100);
     }
@@ -41,9 +40,9 @@ unsafe fn read24(i2c: &mut I2c, reg: u8) -> u32 {
 
     i2c.write_read(0x76, &[reg], &mut buf).unwrap();
     let mut val = 0;
-    val |= (buf[0] as u32) << 0;
+    val |= (buf[2] as u32) << 0;
     val |= (buf[1] as u32) << 8;
-    val |= (buf[2] as u32) << 16;
+    val |= (buf[0] as u32) << 16;
 
     val
 }
